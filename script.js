1f74837c8e4d34255e9470bad539223fa172722d
Joy Bhalla <joybh@windows.localdomain>
2022-10-05 13:58:47 +0530

    added fetch to get the details as json

--- script.js
+++ script.js
@@ -2,5 +2,14 @@ let username="";
 document.querySelector(".input").addEventListener("change",function(event) {
     event.preventDefault();
     username=document.getElementById("username").value;
-    console.log(username);
+    //console.log(username);
+    
+    const baseUrl="https://api.github.com/users/";
+
+    const fullUrl=baseUrl+username;
+
+    // get user endpoint, then getting the response as json
+    fetch(fullUrl).then(res=>res.json())
+    // as json is our actual data check if we have the right data
+    .then(data=>console.log(data));
 });
