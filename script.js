77bbdfd6df329e17f858332821bbda178abd284f
Joy Bhalla <joybh@windows.localdomain>
2022-10-07 14:31:18 +0530

    added fetch promise then catch to get details in json

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 let username="";
+
 document.querySelector(".input").addEventListener("change",function(event) {
     event.preventDefault();
     username=document.getElementById("username").value;
     //console.log(username);
-    
+
+    let userdata= {
+        name:"",
+        email:"",
+        followers:0,
+    };
+
     const baseUrl="https://api.github.com/users/";
 
     const fullUrl=baseUrl+username;
@@ -11,5 +18,16 @@ document.querySelector(".input").addEventListener("change",function(event) {
     // get user endpoint, then getting the response as json
     fetch(fullUrl).then(res=>res.json())
     // as json is our actual data check if we have the right data
-    .then(data=>console.log(data));
+    .then(
+        data=>{
+            userdata.name=data.name;
+            userdata.email=data.email;
+            userdata.followers=data.followers
+        }).then(
+            let div = document.createElement('div');
+            div.className="card";
+            let text=document.createTextNode(userdata);
+            div.appendChild(text);
+            document.body.appendChild(div);
+        );
 });
